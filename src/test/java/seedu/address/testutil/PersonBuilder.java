96f417948d9f589460b1f01af40b0eaf00f74263
Yang Shuqi <shu7yang@gmail.com>
2018-10-03 01:47:45 +0800

    Create Solution and SolutionLink model

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,11 +3,8 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
+import seedu.address.model.issue.*;
 import seedu.address.model.issue.Email;
-import seedu.address.model.issue.Issue;
-import seedu.address.model.issue.IssueStatement;
-import seedu.address.model.issue.Phone;
-import seedu.address.model.issue.Remark;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 

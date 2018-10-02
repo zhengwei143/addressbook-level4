dba903d04be6ee1d1ec3f8bfea60ded1cd835478
Jason <fwenkai.jason@gmail.com>
2018-10-02 20:38:01 +0800

    rearrange imports order

--- src/test/java/seedu/address/AppParametersTest.java
+++ src/test/java/seedu/address/AppParametersTest.java
@@ -8,9 +8,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javafx.application.Application;
-
 import org.junit.Test;
+
+import javafx.application.Application;
 
 public class AppParametersTest {
 

6493d2b0b4d96d09cb06fd5b381a4bf6e6539005
xllliu <E0134148@u.nus.edu>
2018-10-02 23:45:57 +0800

    Change the message of find command and list command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -6,13 +6,13 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all issues in the address book to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all issues";
 
 
     @Override

e8cbe80c55f0ae0ba98ead2c76448632e2944260
Yang Shuqi <shu7yang@gmail.com>
2018-10-17 03:18:07 +0800

    fix nits

--- src/test/java/seedu/address/storage/XmlSerializableSaveItTest.java
+++ src/test/java/seedu/address/storage/XmlSerializableSaveItTest.java
@@ -17,24 +17,24 @@ import seedu.address.testutil.TypicalPersons;
 public class XmlSerializableSaveItTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableSaveItTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsSaveIt.xml");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonSaveIt.xml");
+    private static final Path TYPICAL_ISSUES_FILE = TEST_DATA_FOLDER.resolve("typicalIssuesSaveIt.xml");
+    private static final Path DUPLICATE_ISSUE_FILE = TEST_DATA_FOLDER.resolve("duplicateIssuesSaveIt.xml");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
-        XmlSerializableSaveIt dataFromFile = XmlUtil.getDataFromFile(TYPICAL_PERSONS_FILE,
+        XmlSerializableSaveIt dataFromFile = XmlUtil.getDataFromFile(TYPICAL_ISSUES_FILE,
                 XmlSerializableSaveIt.class);
         SaveIt saveItFromFile = dataFromFile.toModelType();
-        SaveIt typicalPersonsSaveIt = TypicalPersons.getTypicalSaveIt();
-        assertEquals(saveItFromFile, typicalPersonsSaveIt);
+        SaveIt typicalIssuesSaveIt = TypicalPersons.getTypicalSaveIt();
+        assertEquals(saveItFromFile, typicalIssuesSaveIt);
     }
 
     @Test
     public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
-        XmlSerializableSaveIt dataFromFile = XmlUtil.getDataFromFile(DUPLICATE_PERSON_FILE,
+        XmlSerializableSaveIt dataFromFile = XmlUtil.getDataFromFile(DUPLICATE_ISSUE_FILE,
                 XmlSerializableSaveIt.class);
         thrown.expect(IllegalValueException.class);
         thrown.expectMessage(XmlSerializableSaveIt.MESSAGE_DUPLICATE_PERSON);

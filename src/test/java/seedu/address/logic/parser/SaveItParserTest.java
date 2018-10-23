e1a6dfc65ae8fccfbdd45a818f2d25162f20c13f
Yang Shuqi <shu7yang@gmail.com>
2018-10-23 23:41:52 +0800

    remove unused import

--- src/test/java/seedu/address/logic/parser/SaveItParserTest.java
+++ src/test/java/seedu/address/logic/parser/SaveItParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;

2dadf824dcb306a0d6de8f50db8a950f4e0a5245
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2018-10-24 15:31:25 +0800

    implement the RetrieveCommand.java

--- src/main/java/seedu/address/logic/commands/RetrieveCommand.java
+++ src/main/java/seedu/address/logic/commands/RetrieveCommand.java
@@ -2,10 +2,16 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Issue;
 import seedu.address.model.Model;
 
 public class RetrieveCommand extends Command{
@@ -17,19 +23,52 @@ public class RetrieveCommand extends Command{
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_RETRIEVE_LINK_SUCCESS = "Solution link no. %1$s is pasted to the clipboard.";
+    private static final String MESSAGE_RETRIEVE_LINK_SUCCESS = "Solution link no. %1$s is pasted to the clipboard.";
+    private static final String MESSAGE_FAILED_SELECTION = "Issue has to be selected first before retrieving solution link.";
+    private static final String MESSAGE_FAILED_SOLUTION = "The solution index provided is invalid";
 
     private final Index targetedIndex;
 
+    private Clipboard clipboard;
+
     public RetrieveCommand(Index targetedIndex) {
         this.targetedIndex = targetedIndex;
+        this.clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history)
-            throws CommandException, ParseException {
+            throws CommandException{
         requireNonNull(model);
 
-        return null;
+        int issueIndex = model.getCurrentDirectory();
+        List<Issue> lastShownList = model.getFilteredIssueList();
+        Issue selectedIssue;
+
+        if (issueIndex > 0) {
+            selectedIssue = lastShownList.get(issueIndex-1);
+            try {
+                String selectedLink = selectedIssue.getSolutions().get(targetedIndex.getZeroBased()).getLink().value;
+                copyToClipBoard(selectedLink);
+                return new CommandResult(String.format(MESSAGE_RETRIEVE_LINK_SUCCESS, targetedIndex.getOneBased()));
+            } catch (IndexOutOfBoundsException e) {
+                throw new CommandException(MESSAGE_FAILED_SOLUTION);
+            }
+        } else {
+            throw new CommandException(MESSAGE_FAILED_SELECTION);
+        }
+    }
+
+    private void copyToClipBoard (String solution) {
+        Clipboard clipBoard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection selection = new StringSelection(solution);
+        clipBoard.setContents(selection, null);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof RetrieveCommand // instanceof handles nulls
+                && targetedIndex.equals(((RetrieveCommand) other).targetedIndex));
     }
 }

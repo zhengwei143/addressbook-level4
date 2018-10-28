0151534b28f07710dba5c1d79f15f53735b8ccbb
xllliu <E0134148@u.nus.edu>
2018-10-28 19:18:13 +0800

    Display current directory in ResultDisplay

---
+++ src/main/java/seedu/saveit/commons/core/directory/Directory.java
@@ -0,0 +1,9 @@
+package seedu.saveit.commons.core.directory;
+
+/**
+ * Represent the directory of current edition by user. It can be passed as parameters to communicate with other
+ * components as the directory design is rather complicated. There are three levels: root/issue/solution.
+ */
+public class Directory {
+    private int level
+}
--- src/main/java/seedu/saveit/ui/ResultDisplay.java
+++ src/main/java/seedu/saveit/ui/ResultDisplay.java
@@ -8,9 +8,11 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.saveit.commons.core.LogsCenter;
+import seedu.saveit.commons.events.ui.JumpToListRequestEvent;
 import seedu.saveit.commons.events.ui.NewResultAvailableEvent;
 
 /**
@@ -20,15 +22,21 @@ public class ResultDisplay extends UiPart<Region> {
 
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
+    private static final String ROOT_DIRECTORY = "../SaveIt";
 
     private final StringProperty displayed = new SimpleStringProperty("");
+    private final StringProperty currentDirectory = new SimpleStringProperty(ROOT_DIRECTORY);
 
     @FXML
     private TextArea resultDisplay;
 
+    @FXML
+    private Label directory;
+
     public ResultDisplay() {
         super(FXML);
         resultDisplay.textProperty().bind(displayed);
+        directory.textProperty().bind(currentDirectory);
         registerAsAnEventHandler(this);
     }
 
@@ -36,6 +44,12 @@ public class ResultDisplay extends UiPart<Region> {
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         Platform.runLater(() -> displayed.setValue(event.message));
+    }
+
+    @Subscribe
+    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        Platform.runLater(() -> currentDirectory.setValue(ROOT_DIRECTORY + "/issue " + (event.targetIndex + 1)));
     }
 
 }

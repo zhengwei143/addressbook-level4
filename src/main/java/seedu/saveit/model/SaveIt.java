6cf9259b50700a960f9aa0ac23d2a7f7b1247809
jasonvank <fwenkai.jason@gmail.com>
2018-11-02 01:39:38 +0800

    I dont want to fix travis warning anymore!!!!

--- src/main/java/seedu/saveit/model/SaveIt.java
+++ src/main/java/seedu/saveit/model/SaveIt.java
@@ -19,8 +19,8 @@ import seedu.saveit.model.issue.Tag;
 public class SaveIt implements ReadOnlySaveIt {
 
     private final UniqueIssueList issues;
+    private final String DUMMY_TAG = "dummyTag";
     private Directory currentDirectory;
-    private static final String DUMMY_TAG = "dummyTag";
 
 
     /*

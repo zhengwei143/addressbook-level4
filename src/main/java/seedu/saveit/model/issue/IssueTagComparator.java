81848a3d888f8602ab7c2cedd6b5a5e09b1aec98
Yang Shuqi <shu7yang@gmail.com>
2018-11-06 00:50:23 +0800

    update tag comparator

--- src/main/java/seedu/saveit/model/issue/IssueTagComparator.java
+++ src/main/java/seedu/saveit/model/issue/IssueTagComparator.java
@@ -1,6 +1,8 @@
 package seedu.saveit.model.issue;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
 
 import seedu.saveit.model.Issue;
 
@@ -12,19 +14,18 @@ public class IssueTagComparator implements Comparator<Issue> {
      * Compare Issue a and b with their first tag in the tag set. Tag is compared based on String tagName.
      */
     public int compare(Issue a, Issue b) {
-        Tag tagB = b.getTags().stream().findFirst().orElse(null);
-        Tag tagA = a.getTags().stream().findFirst().orElse(null);
+        Set<Tag> tagsB = b.getTags();
+        Set<Tag> tagsA = a.getTags();
 
-        if (tagA == null) {
-            return 1;
+        Iterator<Tag> tagAIterator = tagsA.iterator();
+        Iterator<Tag> tagBIterator = tagsB.iterator();
+        while (tagAIterator.hasNext() && tagBIterator.hasNext()) {
+            Tag tagA = tagAIterator.next();
+            Tag tagB = tagBIterator.next();
+            if (tagA.compare(tagB) != 0) {
+                return tagA.compare(tagB);
+            }
         }
-
-        int result = tagA.compare(tagB);
-
-        if (result == 0) {
-            return a.getTags().size() - b.getTags().size();
-        } else {
-            return result;
-        }
+        return tagsA.size() - tagsB.size();
     }
 }

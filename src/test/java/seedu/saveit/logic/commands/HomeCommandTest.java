c42f53de8b2af25fafc960fee861ad0cc051110f
xllliu <E0134148@u.nus.edu>
2018-11-06 21:49:34 +0800

    Add HomeCommandTest

---
+++ src/test/java/seedu/saveit/logic/commands/HomeCommandTest.java
@@ -0,0 +1,33 @@
+package seedu.saveit.logic.commands;
+
+import static org.junit.Assert.assertTrue;
+import static seedu.saveit.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.saveit.logic.commands.HomeCommand.MESSAGE_SUCCESS;
+import static seedu.saveit.testutil.TypicalIssues.getTypicalSaveIt;
+
+import org.junit.Test;
+
+import seedu.saveit.commons.events.ui.ChangeDirectoryRequestEvent;
+import seedu.saveit.logic.CommandHistory;
+import seedu.saveit.model.Model;
+import seedu.saveit.model.ModelManager;
+import seedu.saveit.model.UserPrefs;
+import seedu.saveit.ui.testutil.EventsCollectorRule;
+
+public class HomeCommandTest {
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    private Model model = new ModelManager(getTypicalSaveIt(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalSaveIt(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_home_success() {
+        assertCommandSuccess(new HomeCommand(), model, commandHistory, MESSAGE_SUCCESS, expectedModel);
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ChangeDirectoryRequestEvent);
+        assertTrue(eventsCollectorRule.eventsCollector.getSize() == 2);
+        ChangeDirectoryRequestEvent lastEvent =
+                (ChangeDirectoryRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertTrue(lastEvent.directory.isRootLevel());
+    }
+}

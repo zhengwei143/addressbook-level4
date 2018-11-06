00c93b74f02a97d9cdbbb993a17a2853d2e7a8eb
Yang Shuqi <shu7yang@gmail.com>
2018-11-06 18:34:56 +0800

    update nits

--- src/test/java/seedu/saveit/logic/parser/SetPrimaryCommandParserTest.java
+++ src/test/java/seedu/saveit/logic/parser/SetPrimaryCommandParserTest.java
@@ -19,9 +19,13 @@ public class SetPrimaryCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, "-1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, "1 abc", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "-1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "0",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 abc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetPrimaryCommand.MESSAGE_USAGE));
     }
 }

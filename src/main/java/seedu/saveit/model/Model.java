b4d6d0b6b034633cb973ac9703a4fd34c645f6bd
Yang Shuqi <shu7yang@gmail.com>
2018-11-09 21:36:03 +0800

    remove redundant method in interface

--- src/main/java/seedu/saveit/model/Model.java
+++ src/main/java/seedu/saveit/model/Model.java
@@ -25,9 +25,6 @@ public interface Model {
     /** Reset the current directory. */
     void resetDirectory(Directory currentDirectory);
 
-    /** Reset the current sortType. */
-    void resetSortType(Comparator<Issue> currentSortType);
-
     /** Return the current directory. */
     Directory getCurrentDirectory();
 

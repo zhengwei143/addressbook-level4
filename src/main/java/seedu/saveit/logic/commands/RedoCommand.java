bd9cae09b63dba84f1e15ef6f2cdd5983552512d
xllliu <E0134148@u.nus.edu>
2018-11-10 16:22:09 +0800

    Change the app to root directory after undo and redo

--- src/main/java/seedu/saveit/logic/commands/RedoCommand.java
+++ src/main/java/seedu/saveit/logic/commands/RedoCommand.java
@@ -2,6 +2,9 @@ package seedu.saveit.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.saveit.commons.core.EventsCenter;
+import seedu.saveit.commons.core.directory.Directory;
+import seedu.saveit.commons.events.model.DirectoryChangedEvent;
 import seedu.saveit.logic.CommandHistory;
 import seedu.saveit.logic.commands.exceptions.CommandException;
 import seedu.saveit.model.Model;
@@ -27,6 +30,8 @@ public class RedoCommand extends Command {
 
         model.redoSaveIt();
         model.updateFilteredIssueList(Model.PREDICATE_SHOW_ALL_ISSUES);
+        model.resetDirectory(new Directory(0, 0));
+        EventsCenter.getInstance().post(new DirectoryChangedEvent(new Directory(0, 0)));
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }

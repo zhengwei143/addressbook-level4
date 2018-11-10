e35f6fdbac894f0023a12c550cf429e277561928
xllliu <E0134148@u.nus.edu>
2018-11-10 17:58:40 +0800

    Add ResultDisplayTest

--- src/test/java/guitests/guihandles/ResultDisplayHandle.java
+++ src/test/java/guitests/guihandles/ResultDisplayHandle.java
@@ -1,22 +1,37 @@
 package guitests.guihandles;
 
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 /**
  * A handler for the {@code ResultDisplay} of the UI
  */
-public class ResultDisplayHandle extends NodeHandle<TextArea> {
+public class ResultDisplayHandle extends NodeHandle<Node> {
 
     public static final String RESULT_DISPLAY_ID = "#resultDisplay";
+    public static final String DIRECTORY_FIELD_ID = "#directory";
 
-    public ResultDisplayHandle(TextArea resultDisplayNode) {
+    private final TextArea resultDisplay;
+    private final Label directory;
+
+    public ResultDisplayHandle(Node resultDisplayNode) {
         super(resultDisplayNode);
+        resultDisplay = getChildNode(RESULT_DISPLAY_ID);
+        directory = getChildNode(DIRECTORY_FIELD_ID);
     }
 
     /**
      * Returns the text in the result display.
      */
     public String getText() {
-        return getRootNode().getText();
+        return resultDisplay.getText();
+    }
+
+    /**
+     * Returns the text in the currentDirectory in the result diaplay.
+     */
+    public String getDirectory() {
+        return directory.getText();
     }
 }
--- src/test/java/seedu/saveit/ui/ResultDisplayTest.java
+++ src/test/java/seedu/saveit/ui/ResultDisplayTest.java
@@ -2,16 +2,24 @@ package seedu.saveit.ui;
 
 import static org.junit.Assert.assertEquals;
 import static seedu.saveit.testutil.EventsUtil.postNow;
+import static seedu.saveit.testutil.TypicalDirectories.ISSUE_LEVEL;
+import static seedu.saveit.testutil.TypicalDirectories.ROOT_LEVEL;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import guitests.guihandles.ResultDisplayHandle;
+import seedu.saveit.commons.events.model.DirectoryChangedEvent;
 import seedu.saveit.commons.events.ui.NewResultAvailableEvent;
+
 
 public class ResultDisplayTest extends GuiUnitTest {
 
     private static final NewResultAvailableEvent NEW_RESULT_EVENT_STUB = new NewResultAvailableEvent("Stub");
+    private static final DirectoryChangedEvent  DIRECTORY_CHANGED_ROOT_EVENT_STUB =
+            new DirectoryChangedEvent(ROOT_LEVEL);
+    private static final DirectoryChangedEvent  DIRECTORY_CHANGED_ISSUE_EVENT_STUB =
+            new DirectoryChangedEvent(ISSUE_LEVEL);
 
     private ResultDisplayHandle resultDisplayHandle;
 
@@ -20,8 +28,7 @@ public class ResultDisplayTest extends GuiUnitTest {
         ResultDisplay resultDisplay = new ResultDisplay();
         uiPartRule.setUiPart(resultDisplay);
 
-        resultDisplayHandle = new ResultDisplayHandle(getChildNode(resultDisplay.getRoot(),
-                ResultDisplayHandle.RESULT_DISPLAY_ID));
+        resultDisplayHandle = new ResultDisplayHandle(resultDisplay.getRoot());
     }
 
     @Test
@@ -34,5 +41,15 @@ public class ResultDisplayTest extends GuiUnitTest {
         postNow(NEW_RESULT_EVENT_STUB);
         guiRobot.pauseForHuman();
         assertEquals(NEW_RESULT_EVENT_STUB.message, resultDisplayHandle.getText());
+
+        //directory changed to root
+        postNow(DIRECTORY_CHANGED_ROOT_EVENT_STUB);
+        guiRobot.pauseForHuman();;
+        assertEquals(ROOT_LEVEL.toString(), resultDisplayHandle.getDirectory());
+
+        //directory changed to issue
+        postNow(DIRECTORY_CHANGED_ISSUE_EVENT_STUB);
+        guiRobot.pauseForHuman();
+        assertEquals(ISSUE_LEVEL.toString(), resultDisplayHandle.getDirectory());
     }
 }

3fccb6859ea9f10e9bd484eaff5b48a0be299a72
Yang Shuqi <shu7yang@gmail.com>
2018-11-10 15:11:20 +0800

    update dispaly pane

--- src/main/java/seedu/saveit/ui/ResultDisplay.java
+++ src/main/java/seedu/saveit/ui/ResultDisplay.java
@@ -11,7 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
-import org.controlsfx.control.StatusBar;
+
 import seedu.saveit.commons.core.LogsCenter;
 import seedu.saveit.commons.events.model.DirectoryChangedEvent;
 import seedu.saveit.commons.events.model.SortTypeChangedEvent;

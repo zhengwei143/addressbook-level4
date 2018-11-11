a24982c194ce38838317381892acc259a8c75f96
zhengwei <tanzhengwei143@gmail.com>
2018-11-12 03:21:44 +0800

    Attempt to fix SortCommandTest

--- src/test/java/seedu/saveit/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/saveit/logic/commands/SortCommandTest.java
@@ -92,7 +92,7 @@ public class SortCommandTest {
         String expectedMessage = String.format(SortCommand.MESSAGE_SUCCESS, sortType.getSortType());
         Issue issue = new Issue(new IssueStatement("new SOLUTION_C++ problem"),
                 new Description("only for test"), new ArrayList<>(), new HashSet<>());
-        updateFrequency(JAVA_NULL_POINTER, JAVA_NULL_POINTER, JAVA_NULL_POINTER, JAVA_NULL_POINTER,
+        updateFrequency(JAVA_NULL_POINTER, JAVA_NULL_POINTER, JAVA_NULL_POINTER, JAVA_NULL_POINTER, JAVA_NULL_POINTER,
                 C_SEGMENTATION_FAULT, C_SEGMENTATION_FAULT, C_SEGMENTATION_FAULT, C_SEGMENTATION_FAULT,
                 TRAVIS_BUILD, TRAVIS_BUILD, TRAVIS_BUILD,
                 RUBY_HASH_BUG, CHECKSTYLE_ERROR, C_SEGMENTATION_FAULT, QUICKSORT_BUG);
@@ -102,7 +102,7 @@ public class SortCommandTest {
         model.addIssue(issue);
         SortCommand command = new SortCommand(sortType);
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(C_SEGMENTATION_FAULT, JAVA_NULL_POINTER, TRAVIS_BUILD, RUBY_HASH_BUG,
+        assertEquals(Arrays.asList(JAVA_NULL_POINTER, C_SEGMENTATION_FAULT, TRAVIS_BUILD, RUBY_HASH_BUG,
                 CHECKSTYLE_ERROR, QUICKSORT_BUG, C_RACE_CONDITION, issue),
                 model.getFilteredAndSortedIssueList());
     }

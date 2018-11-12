2379e34b40323724a33a9aba5cbe29261ad2b032
jasonvank <fwenkai.jason@gmail.com>
2018-11-12 11:49:56 +0800

    improve command highlight ability

--- src/main/java/seedu/saveit/ui/CommandHighlightManager.java
+++ src/main/java/seedu/saveit/ui/CommandHighlightManager.java
@@ -1,5 +1,6 @@
 package seedu.saveit.ui;
 
+import static seedu.saveit.logic.parser.CliSyntax.PREFIX_SOLUTION_LINK_STRING;
 import static seedu.saveit.model.util.SampleDataUtil.isCommandWordNeedIndex;
 import static seedu.saveit.model.util.SampleDataUtil.isPrefixParameter;
 
@@ -10,7 +11,7 @@ import org.fxmisc.richtext.InlineCssTextArea;
  */
 public class CommandHighlightManager {
 
-    public static final String STYLE_COMMAND_WORD = "-fx-fill: #f4ad42;";
+    public static final String STYLE_COMMAND_WORD = "-fx-fill: #e2a03d;";
     public static final String STYLE_PARAMETER_KEY = "-fx-fill: #ffff00;";
     public static final String STYLE_INDEX = "-fx-fill: #55ae47;";
     public static final String STYLE_NORMAL_VALUE = "-fx-fill: #42c3f4;";
@@ -23,7 +24,8 @@ public class CommandHighlightManager {
         StringBuilder commandWord = new StringBuilder();
         int position = 0;
         boolean indexHighlighted = false;
-        boolean indexNeedHighlight;
+        boolean indexNeedToHighlight;
+        boolean isSolutionLink = false;
 
         // if there are space chars before command word, pos++
         while (isShorterThanInput(userInput, position) && isSpace(userInput, position)) {
@@ -38,21 +40,23 @@ public class CommandHighlightManager {
         }
 
         // check for some command word that does not require index
-        indexNeedHighlight = checkCommandWord(commandWord);
+        indexNeedToHighlight = checkCommandWord(commandWord);
 
         // highlight the following parameters, which are key-value pairs
         while (isShorterThanInput(userInput, position)) {
-            while (indexNeedHighlight && !indexHighlighted && isIndex(userInput, position) && !isSpace(userInput,
+            while (indexNeedToHighlight && !indexHighlighted && isIndex(userInput, position) && !isSpace(userInput,
                 position)) {
                 commandTextField.setStyle(position, position + 1, STYLE_INDEX);
                 position++;
             }
 
-            if (isShorterThanInput(userInput, position) && isParameter(userInput, position)) {
+            if (!isSolutionLink && isShorterThanInput(userInput, position) && isParameter(userInput, position)) {
                 commandTextField.setStyle(position - 1, position + 1, STYLE_PARAMETER_KEY);
                 position++;
                 indexHighlighted = true;
             }
+
+            isSolutionLink = isSolutionLinkStart(userInput, position, isSolutionLink);
 
             if (!isShorterThanInput(userInput, position)) {
                 break;
@@ -60,7 +64,35 @@ public class CommandHighlightManager {
 
             commandTextField.setStyle(position, position + 1, STYLE_NORMAL_VALUE);
             position++;
+
+            isSolutionLink = isSolutionLinkEnd(userInput, position, isSolutionLink);
         }
+    }
+
+
+    /**
+     * This method is used to check whether user is currently input the solution link, return true if solution link
+     * prefix is found.
+     */
+    private static boolean isSolutionLinkStart(String userInput, int position, boolean isSolutionLink) {
+        if (userInput.charAt(position - 1) == '/') {
+            String inputCheck = getString(userInput, position, 2, position - 1);
+            if (inputCheck.equals(PREFIX_SOLUTION_LINK_STRING)) {
+                isSolutionLink = true;
+            }
+        }
+        return isSolutionLink;
+    }
+
+    /**
+     * This method is used to check whether the user finishes solution link by checking whether space is input, return
+     * false if it is not solution link, otherwise return true.
+     */
+    private static boolean isSolutionLinkEnd(String userInput, int position, boolean isSolutionLink) {
+        if (isSolutionLink && isSpace(userInput, position - 1)) {
+            isSolutionLink = false;
+        }
+        return isSolutionLink;
     }
 
     /**
@@ -94,14 +126,18 @@ public class CommandHighlightManager {
      */
     private static boolean isParameter(String userInput, int position) {
         if (userInput.charAt(position) == '/') {
-            StringBuilder input = new StringBuilder();
-            input.append(userInput.charAt(position - 1));
-            input.append(userInput.charAt(position));
-            String inputCheck = input.toString();
+            String inputCheck = getString(userInput, position, 1, position);
             if (isPrefixParameter(inputCheck)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static String getString(String userInput, int position, int i, int i2) {
+        StringBuilder input = new StringBuilder();
+        input.append(userInput.charAt(position - i));
+        input.append(userInput.charAt(i2));
+        return input.toString();
     }
 }
